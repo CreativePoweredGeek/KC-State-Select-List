043dd3be891590df36ba912b3059241dbe0f556f
Russ Kern <russ@russkern.com>
2017-07-28 20:37:00 -0400

    Initial Commit
    Incomplete. Started the base Structure.

---
+++ addon.setup.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'author'         => 'KernCreative',
+	'author_url'     => 'http://russkern.com/',
+	'docs_url'       => 'http://www.russkern.com/',
+	'name'           => 'KC State Select List',
+	'description'    => 'Simple function to produce a state select list',
+	'version'        => '0.0.1',
+	'namespace'      => 'KernCreative\KC-State-Select-List',
+	'settings_exist' => FALSE
+);
